b6335eae6723f7b1f09b3cb26e366ac0ae87c59e
folex <0xdxdy@gmail.com>
2023-01-19 16:56:48 +0700

    add abstract interface

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,1 +1,5 @@
 export const add = (a: number, b: number) => a + b
+
+export abstract class Interface {
+    abstract helloWorld(): string;
+}
--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -3,12 +3,31 @@ import { Hello } from '@test/ui'
 import { add } from '@test/utils'
 import ReactDOM from 'react-dom'
 
+import { Interface } from "@test/utils";
+
+export class Consumer extends Interface {
+  name: string;
+
+  constructor(a: string) {
+    super();
+    this.name = a;
+  }
+
+  helloWorld(): string {
+    const demo = `hello i'm consumer ${this.name}`;
+    console.log(demo);
+    return demo;
+  }
+}
+
 export const App = () => {
   return (
     <div>
       <Hello />
       <br />
       {add(1, 2)}
+      <br />
+      {new Consumer("consumer-name").helloWorld()}
     </div>
   )
 }
