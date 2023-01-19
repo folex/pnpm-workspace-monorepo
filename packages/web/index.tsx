c3830a82a9a329b81415445eeb4fea1d204b07e0
Pavel Murygin <pavel.murygin@gmail.com>
2023-01-19 15:28:32 +0400

    trying to use Fluence in web app :)

--- packages/web/index.tsx
+++ packages/web/index.tsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import { Hello } from '@test/ui'
-import { add } from '@test/utils'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
-import { Interface } from "@test/utils";
+import { Fluence } from '@fluencelabs/fluence'
+import { Hello } from '@test/ui'
+import { add } from '@test/utils'
+import { Interface } from '@test/utils'
 
 export class Consumer extends Interface {
-  name: string;
+  name: string
 
   constructor(a: string) {
-    super();
-    this.name = a;
+    super()
+    this.name = a
   }
 
   helloWorld(): string {
-    const demo = `hello i'm consumer ${this.name}`;
-    console.log(demo);
-    return demo;
+    const demo = `hello i'm consumer ${this.name}`
+    console.log(demo)
+    return demo
   }
 }
 
 export const App = () => {
+  useEffect(() => {
+    Fluence.start()
+  })
+
   return (
     <div>
       <Hello />
       <br />
       {add(1, 2)}
       <br />
-      {new Consumer("consumer-name").helloWorld()}
+      {new Consumer('consumer-name').helloWorld()}
     </div>
   )
 }
