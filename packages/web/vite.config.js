891f8a5354b57b78c2482ae3713c65571510b7ab
Pavel Murygin <pavel.murygin@gmail.com>
2023-01-19 15:55:53 +0400

    use polyfills

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
 export default defineConfig({
   base: '',
   plugins: [tsconfigPaths()],
+  define: {
+    // Node.js global to browser globalThis
+    define: {
+      global: 'globalThis',
+    },
+    // Enable esbuild polyfill plugins
+    plugins: [
+      NodeGlobalsPolyfillPlugin({
+        buffer: true,
+        process: true,
+      }),
+    ],
+  },
 })
