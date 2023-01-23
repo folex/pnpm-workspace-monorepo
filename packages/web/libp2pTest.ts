5292c7c9a69b7f5bc1c5fdd3ec18b97524491a1c
Pavel Murygin <pavel.murygin@gmail.com>
2023-01-23 15:50:37 +0400

    latest version of libp2p

--- packages/web/libp2pTest.ts
+++ packages/web/libp2pTest.ts
@@ -1,47 +1,46 @@
 import { encode } from 'it-length-prefixed'
 import { pipe } from 'it-pipe'
 import { createLibp2p } from 'libp2p'
-// @ts-ignore
-import Mplex from 'libp2p-mplex'
-// @ts-ignore
-import Websockets from 'libp2p-websockets'
-// @ts-ignore
-import { all as allow_all } from 'libp2p-websockets/src/filters'
 
-import { Noise } from '@chainsafe/libp2p-noise'
+import { noise } from '@chainsafe/libp2p-noise'
 import { krasnodar } from '@fluencelabs/fluence-network-environment'
-import { createPeerId } from '@libp2p/peer-id'
-import { Multiaddr } from '@multiformats/multiaddr'
+import { mplex } from '@libp2p/mplex'
+import { createEd25519PeerId } from '@libp2p/peer-id-factory'
+import { webSockets } from '@libp2p/websockets'
+import { all } from '@libp2p/websockets/filters'
+import { multiaddr } from '@multiformats/multiaddr'
 
 export const PROTOCOL_NAME = '/fluence/particle/2.0.0'
 
 export const testLibp2p = async () => {
-  const transportKey = Websockets.prototype[Symbol.toStringTag]
-  const peerId = await PeerId.create({ keyType: 'Ed25519' })
+  const peerId = await createEd25519PeerId()
   const lib2p2Peer = await createLibp2p({
     peerId,
-    modules: {
-      transport: [Websockets],
-      streamMuxer: [Mplex],
-      connEncryption: [new Noise()],
-    },
-    config: {
-      transport: {
-        [transportKey]: {
-          filter: allow_all,
-        },
-      },
-    },
+    transports: [
+      webSockets({
+        filter: all,
+      }),
+    ],
+    streamMuxers: [mplex],
+    connectionEncryption: [noise],
+
+    // config: {
+    //  transport: {
+    //    [transportKey]: {
+    //      filter: allow_all,
+    //    },
+    //  },
+    // },
   })
 
   const addr = krasnodar[3]
-  const relayAddress = new Multiaddr(addr.multiaddr)
+  const relayAddress = multiaddr(addr.multiaddr)
 
   await lib2p2Peer.start()
   await lib2p2Peer.dial(relayAddress)
 
-  const conn = await lib2p2Peer.dialProtocol(relayAddress, PROTOCOL_NAME)
-  const sink = conn.stream.sink
+  const stream = await lib2p2Peer.dialProtocol(relayAddress, PROTOCOL_NAME)
+  const sink = stream.sink
 
   pipe(
     // force new line
